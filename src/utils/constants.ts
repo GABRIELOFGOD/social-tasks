d7f826f4c2266fd17878d3dc140dabae09793150
GABRIELOFGOD <gabrielayodele2021@gmail.com>
2025-02-15 02:23:55 +0100

    GREAT IS THY FAITHFULNESS LORD UNTO ME

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,3 +3,4 @@ export const Sub = "/sub.svg";
 
 export const BASEURL = "https://api.ucchain.org";
 // export const BASEURL = "http://localhost:5000";
+// export const BASEURL = process.env.BASEURL;
